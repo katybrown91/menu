165b25f5da2e5b23993d0b6c6253412f6d98d495
Katy Brown <thisis.katybrown@gmail.com>
2019-07-30 16:20:40 -0400

    working

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
 import { Link, NavLink } from 'react-router-dom';
-import { Route, Switch } from 'react-router-dom';
+import {BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from '../src/components/Home';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
      <Switch>
      <Route exact path='/' component={Home} />
      </Switch>
+     </BrowserRouter>
     </div>
   );
 }
